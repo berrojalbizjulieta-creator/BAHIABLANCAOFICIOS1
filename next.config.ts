dd9c572bcb5066ba44002ac8529f04546804e3ca
Agustín Arturo Giardino <agustinarturogiardino@gmail.com>
2025-09-23 18:54:49 +0000

    https://px.cdn.lanueva.com/022023/1677153888501/portal%20otra.jpg?cw=807

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,12 @@ const nextConfig: NextConfig = {
         hostname: 'upload.wikimedia.org',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'px.cdn.lanueva.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
