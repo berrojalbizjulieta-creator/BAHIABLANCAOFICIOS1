f34a227201dc8a2337352de2da7287cd2b42fbac
Agustín Arturo Giardino <agustinarturogiardino@gmail.com>
2025-09-23 17:47:08 +0000

    Try fixing this error: `Console Error: Encountered two children with the

--- src/components/admin/recent-sales.tsx
+++ src/components/admin/recent-sales.tsx
@@ -12,8 +12,11 @@ export function RecentSales() {
 
   return (
     <div className="space-y-8">
-        {recentUsers.map(user => (
-            <div key={user.id} className="flex items-center">
+        {recentUsers.map(user => {
+            const isProfessional = 'avgRating' in user;
+            const key = isProfessional ? `pro-${user.id}` : `client-${user.id}`;
+            return (
+            <div key={key} className="flex items-center">
                 <Avatar className="h-9 w-9">
                 <AvatarImage src={user.photoUrl} alt={user.name} />
                 <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
@@ -24,14 +27,14 @@ export function RecentSales() {
                     {user.email}
                 </p>
                 </div>
-                {'avgRating' in user ? ( // A way to check if user is a Professional
+                {isProfessional ? ( 
                     <div className="ml-auto font-medium">+ $15.000,00</div>
                 ) : (
                     <div className="ml-auto font-medium text-muted-foreground">Cliente</div>
                 )
             }
           </div>
-        ))}
+        )})}
     </div>
   );
 }
