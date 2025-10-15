297dd72c0118c4896a97f2510558b8c186e0a879
Julieta Berrojalbiz <berrojalbizjulieta@gmail.com>
2025-10-15 01:19:01 +0000

    EN LA SECCION ESTAS RESEÑAS LO DCIEN MEJOR EN LA VERSION DE CEUALR DEFOM

--- src/components/professionals/testimonial-slider.tsx
+++ src/components/professionals/testimonial-slider.tsx
@@ -52,8 +52,8 @@ export default function TestimonialSlider({
       </CarouselContent>
         {testimonials.length > 1 && (
         <>
-          <CarouselPrevious className="absolute -left-4 top-1/2 -translate-y-1/2" />
-          <CarouselNext className="absolute -right-4 top-1/2 -translate-y-1/2" />
+          <CarouselPrevious className="absolute -left-4 top-1/2 -translate-y-1/2 hidden md:inline-flex" />
+          <CarouselNext className="absolute -right-4 top-1/2 -translate-y-1/2 hidden md:inline-flex" />
         </>
       )}
     </Carousel>
