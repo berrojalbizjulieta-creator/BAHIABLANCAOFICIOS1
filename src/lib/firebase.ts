05d0c5ddb9db75f2af10e4a67ddf28d12899764f
Agustín Arturo Giardino <agustinarturogiardino@gmail.com>
2025-10-16 05:34:06 +0000

    El Plan Original: La idea era tener una configuración segura. Guardamos

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,17 +4,23 @@ import { getAuth, Auth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
 import { getStorage, FirebaseStorage } from "firebase/storage";
 
+// Solución: Se "hardcodean" las variables para asegurar que estén disponibles durante la compilación.
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAkwEuIqBsIIY_cKmrAkSQhVVUhdrn1xhc",
+  authDomain: "studio-4820039016-5ae38.firebaseapp.com",
+  projectId: "studio-4820039016-5ae38",
+  storageBucket: "studio-4820039016-5ae38.appspot.com",
+  messagingSenderId: "821979814317",
+  appId: "1:821979814317:web:abd23395565fe4e58f6924"
 };
 
 // Inicialización segura de la app de Firebase
-const app: FirebaseApp = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+let app: FirebaseApp;
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
 
 const auth: Auth = getAuth(app);
 const db: Firestore = getFirestore(app);
