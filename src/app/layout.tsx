2cad78e8d3a56ca9d869be7c3f08094b69d0919d
Agustín Arturo Giardino <agustinarturogiardino@gmail.com>
2025-11-06 02:55:59 +0000

    lo hiciste porque te interuurmpoi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,18 +40,6 @@ function AnalyticsManager() {
         window.fbq('track', 'PageView');
     }
 
-    // Custom unique visit counter
-    const sessionKey = 'bbo_visit_counted';
-    if (!sessionStorage.getItem(sessionKey)) {
-      fetch('/api/track-view', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ page: pathname }),
-        keepalive: true,
-      }).catch(console.error);
-      sessionStorage.setItem(sessionKey, 'true');
-    }
-
   }, [pathname, searchParams]);
 
   return null;
@@ -66,42 +54,45 @@ export default function RootLayout({
   return (
     <html lang="es" className="scroll-smooth">
       {/* Scripts for Google Analytics and Meta Pixel */}
-      <>
-        {/* Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-        />
-        <Script id="google-analytics-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-          `}
-        </Script>
-
-        {/* Meta Pixel */}
-        <Script id="meta-pixel-init" strategy="afterInteractive">
-            {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
-            fbq('track', 'PageView');
-            `}
-        </Script>
-        <noscript>
-          <img height="1" width="1" style={{display:'none'}}
-            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          {/* Google Analytics */}
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
           />
-        </noscript>
-      </>
+          <Script id="google-analytics-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+            `}
+          </Script>
+
+          {/* Meta Pixel */}
+          <Script id="meta-pixel-init" strategy="afterInteractive">
+              {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
+              fbq('track', 'PageView');
+              `}
+          </Script>
+          <noscript>
+            <img height="1" width="1" style={{display:'none'}}
+              src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
+        </>
+      )}
+
 
       <body className={`${ptSans.variable} font-body antialiased`}>
         {/* Component to manage navigation events */}
