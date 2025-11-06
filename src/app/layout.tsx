1c785fc2b0f01c11120b6bab08195b85e832635a
Agustín Arturo Giardino <agustinarturogiardino@gmail.com>
2025-11-06 02:48:15 +0000

    ok hacelo y probamos si anda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 'use client';
-// Force Rebuild: Sat Jul 27 2024 22:20:10 GMT+0000 (Coordinated Universal Time)
 
 import { PT_Sans } from 'next/font/google';
 import './globals.css';
@@ -17,17 +16,31 @@ const ptSans = PT_Sans({
   variable: '--font-pt-sans',
 });
 
-function AnalyticsEvents() {
+function AnalyticsManager() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    // Solo se ejecuta en producción para no contaminar las métricas
+    // Only run analytics in production
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
 
-    // 1. Conteo de visitas únicas (lógica existente)
+    const url = pathname + searchParams.toString();
+
+    // Google Analytics Event
+    if (process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && typeof window.gtag === 'function') {
+      window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    }
+
+    // Meta Pixel Event
+    if (process.env.NEXT_PUBLIC_META_PIXEL_ID && typeof window.fbq === 'function') {
+        window.fbq('track', 'PageView');
+    }
+
+    // Custom unique visit counter
     const sessionKey = 'bbo_visit_counted';
     if (!sessionStorage.getItem(sessionKey)) {
       fetch('/api/track-view', {
@@ -37,20 +50,6 @@ function AnalyticsEvents() {
         keepalive: true,
       }).catch(console.error);
       sessionStorage.setItem(sessionKey, 'true');
-    }
-
-    const url = pathname + searchParams.toString();
-
-    // 2. Evento de Google Analytics (GA4)
-    if (typeof window.gtag === 'function') {
-      window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
-        page_path: url,
-      });
-    }
-
-    // 3. Evento de Meta Pixel
-    if (typeof window.fbq === 'function') {
-      window.fbq('track', 'PageView');
     }
 
   }, [pathname, searchParams]);
@@ -66,52 +65,48 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className="scroll-smooth">
-      {/* Scripts para Google Analytics y Meta Pixel (solo en producción) */}
-      {process.env.NODE_ENV === 'production' && (
-        <>
-          {/* Google Analytics */}
-          <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-          />
-          <Script id="google-analytics-init" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
-                page_path: window.location.pathname,
-              });
-            `}
-          </Script>
+      {/* Scripts for Google Analytics and Meta Pixel */}
+      <>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        />
+        <Script id="google-analytics-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+        </Script>
 
-          {/* Meta Pixel */}
-          <Script id="meta-pixel-init" strategy="afterInteractive">
+        {/* Meta Pixel */}
+        <Script id="meta-pixel-init" strategy="afterInteractive">
             {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
-              fbq('track', 'PageView');
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
+            fbq('track', 'PageView');
             `}
-          </Script>
-          <noscript>
-            <img height="1" width="1" style={{display:'none'}}
-              src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
-            />
-          </noscript>
-        </>
-      )}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+      </>
 
       <body className={`${ptSans.variable} font-body antialiased`}>
-        {/* Componente para gestionar los eventos de navegación */}
+        {/* Component to manage navigation events */}
         <Suspense fallback={null}>
-          <AnalyticsEvents />
+          <AnalyticsManager />
         </Suspense>
 
         <div className="flex min-h-screen flex-col">
